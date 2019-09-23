6142b99b585e4ec9d2983414c52cf415ed6ec447
remohofmann <remohofmann@gmx.ch>
2019-09-23 20:07:48 +0200

    initial application

---
+++ src/main/java/ch/bfh/ti/cookingWebapp/CookingWebappApplication.java
@@ -0,0 +1,13 @@
+package ch.bfh.ti.cookingWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingWebappApplication.class, args);
+	}
+
+}
