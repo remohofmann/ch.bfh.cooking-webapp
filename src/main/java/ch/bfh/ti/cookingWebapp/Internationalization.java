5f1df03e9655bfa067602dbd8383337f351c0fa0
lukaszoller <lukas.zoller@gmx.ch>
2019-11-18 13:09:50 +0100

    tried internationalization stuff

--- src/main/java/ch/bfh/ti/cookingWebapp/Internationalization.java
+++ src/main/java/ch/bfh/ti/cookingWebapp/Internationalization.java
@@ -1,21 +1,21 @@
 package ch.bfh.ti.cookingWebapp;
 
-import java.util.Locale;
+        import java.util.Locale;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.LocaleResolver;
+        import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+        import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+        import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class Internationalization extends WebMvcConfigurerAdapter {
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-        sessionLocaleResolver.setDefaultLocale(new Locale("de"));
+        sessionLocaleResolver.setDefaultLocale(new Locale("en"));
         return sessionLocaleResolver;
     }
     @Bean
