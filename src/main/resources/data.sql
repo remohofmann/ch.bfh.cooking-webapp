043f9b97efcdb52b4fe433b46ccd488b9148b266
lukaszoller <lukas.zoller@gmx.ch>
2020-01-03 20:54:44 +0100

    data added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,93 @@ INSERT INTO recipe (recipe_name, recipe_description, recipe_duration) VALUES
   ('Käsespätzle', 'Anbraten, Käse drüber, kochen lassen', 30),
   ('Stroganoff', 'Reis vorkochen, Fleisch anbraten, Gemüse dazu und 20 minuten weiterkochen', 50),
   ('Rindssteak', 'Reis vorkochen, Fleisch anbraten', 30);
+
+INSERT  INTO tag (id, tag_name, tag_type) VALUES
+    ('1', 'Indian', 'cuisine'),
+    ('2', 'Swiss', 'cuisine'),
+    ('3', 'Austrian', 'cuisine'),
+    ('4', 'French', 'cuisine'),
+    ('5', 'Japanese', 'cuisine'),
+    ('6', 'Chinese', 'cuisine'),
+    ('7', 'American', 'cuisine'),
+
+    ('8', 'Gluten free', 'diet'),
+    ('9', 'Vegetarian', 'diet'),
+    ('10', 'Vegan', 'diet'),
+    ('11', 'Paleo', 'diet'),
+
+    ('12', 'Breakfast', 'course'),
+    ('13', 'Lunch', 'course'),
+    ('14', 'Dinner', 'course');
+
+INSERT INTO recipe_tag (recipe_id, tag_id) VALUES
+    ('1', '2'),
+    ('1', '8'),
+    ('1', '9'),
+    ('1', '10'),
+    ('1', '12'),
+
+    ('2', '2'),
+    ('2', '14'),
+
+    ('3', '4'),
+    ('3', '13'),
+
+    ('4', '4'),
+    ('4', '14'),
+
+    ('5', '8'),
+    ('5', '9'),
+    ('5', '10'),
+
+    ('6', '7'),
+    ('6', '11'),
+    ('6', '13'),
+
+    ('7', '7'),
+    ('7', '13'),
+
+    ('8', '11'),
+    ('8', '13'),
+
+    ('9', '4'),
+    ('9', '9'),
+    ('9', '14'),
+
+    ('10', '3'),
+    ('10', '9'),
+
+    ('11', '11'),
+    ('11', '14'),
+
+    ('12', '11'),
+    ('12', '14');
+
+INSERT INTO unit (id, unit_name) VALUES
+    (1, 'Tablespoon');
+
+INSERT INTO ingredient (ingredient_name, ingredient_unit) VALUES
+    ('Milk', 1),
+    ('Eggs', 1),
+    ('Chicken', 1),
+    ('Celery', 1),
+    ('Apples', 1),
+    ('Oranges', 1),
+    ('Rice', 1),
+    ('Noodles', 1),
+    ('Mustard',1),
+    ('Butter',1),
+    ('Oil', 1),
+    ('Olives', 1),
+    ('Garlic', 1),
+    ('Onions', 1),
+    ('Pepper', 1),
+    ('Salt', 1),
+    ('Flour', 1),
+    ('Potatoes', 1),
+    ('Gruyere', 1),
+    ('Beef',1),
+    ('Lemongrass', 1),
+    ('Curry powder', 1),
+    ('Eggplant', 1);
+
